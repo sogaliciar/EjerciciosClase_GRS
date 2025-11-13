9eea34f7eb6b3b7288c7a7a53454566eb5ab5e27
Sofia <so.galicia.r@gmail.com>
2025-11-13 16:25:22 -0600

    ft:Se agregó lógica para botón secuencial y concurrente

--- js/js_concurrencia.js
+++ js/js_concurrencia.js
@@ -27,3 +27,19 @@ sec.onclick = async () =>{
 
     log(`Tiempo total de ejecución: ${((performance.now()-inicio) /1000).toFixed(2)}s`);
 }
+
+//concurrente
+conc.onclick = async()=>{
+    out.textContent="";
+    log("Ejemplo concurrente");
+    const inicio = performance.now();
+
+    await Promise.all(
+        [
+            tarea("Denisse", 3000),
+            tarea("Enrique", 1000)
+        ]
+    );
+
+    log(`Tiempo total de ejecución: ${((performance.now()-inicio) /1000).toFixed(2)}s`);
+}
